fb7a91550672e7eee7b65ba9f3cf6598cb2687be
RACCOOONkim <kimkangtai@naver.com>
2023-05-15 14:02:06 +0900

    ReviseUIInteraction

---
+++ Assets/Scripts/ForSolver.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForSolver : MonoBehaviour
+{
+    public GameObject solver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        solver.SetActive(false);
+    }
+
+    // Update is called once per fram
+}
